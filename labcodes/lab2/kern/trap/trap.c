ce528edb34e865f9df3db08d26e2ce5708e51e31
os_lab_user <you@example.com>
2015-04-01 19:06:30 +0800

    lab1 codes

--- labcodes/lab2/kern/trap/trap.c
+++ labcodes/lab2/kern/trap/trap.c
@@ -34,18 +34,36 @@ static struct pseudodesc idt_pd = {
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
-     /* LAB1 YOUR CODE : STEP 2 */
-     /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
-      *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
-      *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
-      *     (try "make" command in lab1, then you will find vector.S in kern/trap DIR)
-      *     You can use  "extern uintptr_t __vectors[];" to define this extern variable which will be used later.
-      * (2) Now you should setup the entries of ISR in Interrupt Description Table (IDT).
-      *     Can you see idt[256] in this file? Yes, it's IDT! you can use SETGATE macro to setup each item of IDT
-      * (3) After setup the contents of IDT, you will let CPU know where is the IDT by using 'lidt' instruction.
-      *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
-      *     Notice: the argument of lidt is idt_pd. try to find it!
-      */
+	/* LAB1 2012011307 : STEP 2 */
+	     /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
+	      *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
+	      *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
+	      *     (try "make" command in lab1, then you will find vector.S in kern/trap DIR)
+	      *     You can use  "extern uintptr_t __vectors[];" to define this extern variable which will be used later.
+	      * (2) Now you should setup the entries of ISR in Interrupt Description Table (IDT).
+	      *     Can you see idt[256] in this file? Yes, it's IDT! you can use SETGATE macro to setup each item of IDT
+	      * (3) After setup the contents of IDT, you will let CPU know where is the IDT by using 'lidt' instruction.
+	      *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
+	      *     Notice: the argument of lidt is idt_pd. try to find it!
+	      */
+	extern uintptr_t __vectors[];
+	int i;
+	for (i = 0; i < 256; i++)
+	{
+		if (i == T_SYSCALL)
+		{
+			SETGATE(idt[i], 1, GD_KTEXT, __vectors[i], DPL_USER);
+		}
+		else if( i == T_SWITCH_TOK)
+		{
+			SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_USER);
+		}
+		else
+		{
+			SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+		}
+	}
+	lidt(&idt_pd);
 }
 
 static const char *
@@ -134,6 +152,9 @@ print_regs(struct pushregs *regs) {
     cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
 
+
+int tick_cnt = 0;
+
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static void
 trap_dispatch(struct trapframe *tf) {
@@ -147,6 +168,12 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+    	tick_cnt++;
+		if (tick_cnt == TICK_NUM)
+		{
+			print_ticks();
+			tick_cnt = 0;
+		}
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
