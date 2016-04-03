fc3cd6c55169655dba243d97a481806d8fc4b6fb
Bisheng Huang <hbisheng@126.com>
2016-04-03 21:57:04 +0800

    Lab3 fix

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -370,7 +370,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     	pgdir_alloc_page(mm->pgdir, addr, perm);
     } else {
     	if(swap_init_ok) {
-			struct Page *page = pte2page(*ptep);
+			struct Page *page = NULL;
 	    	swap_in(mm, addr, &page);
 	    	page_insert(mm->pgdir, page, addr, perm);
 	    	swap_map_swappable(mm, addr, page, 1);
