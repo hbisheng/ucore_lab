ab0a33d0c742f82265cf4abf506ebe6b1375efef
Bisheng Huang <hbisheng@126.com>
2016-04-03 21:38:46 +0800

    Lab3 ex1 fix

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -365,7 +365,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     *
     */
 
-    ptep = get_page(mm->pgdir, addr, 1);
+    ptep = get_pte(mm->pgdir, addr, 1);
     if(*ptep == 0) {
     	pgdir_alloc_page(mm->pgdir, addr, perm);
     } else {
