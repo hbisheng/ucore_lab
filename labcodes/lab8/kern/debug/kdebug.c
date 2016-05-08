c9dde7fac142109c6cdab04781a341f48f7b18a4
Bisheng Huang <hbisheng@126.com>
2016-05-08 21:03:55 +0800

    Add lab8 codes

--- labcodes/lab8/kern/debug/kdebug.c
+++ labcodes/lab8/kern/debug/kdebug.c
@@ -347,5 +347,23 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+	uint32_t ebp = read_ebp();
+	uint32_t eip = read_eip();
+	int s;
+	for( s = 0; s < STACKFRAME_DEPTH; s++)
+	{
+		//if (ebp == 0) break;
+		cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);
+		int i;
+		for ( i = 0; i < 4;  i++)
+		{
+			cprintf("0x%08x ", ((uint32_t *)ebp + 2)[i]);
+			if(i == 3)
+				cprintf("\n");
+		}
+		print_debuginfo(eip - 1);
+		eip = *((uint32_t *)ebp + 1);
+		ebp = *((uint32_t *)ebp + 0);
+	}
 }
 
--- labcodes/lab8/kern/mm/vmm.c
+++ labcodes/lab8/kern/mm/vmm.c
@@ -493,6 +493,26 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
         }
    }
 #endif
+    ptep = get_pte(mm->pgdir, addr, 1);
+	if(*ptep == 0) {
+		pgdir_alloc_page(mm->pgdir, addr, perm);
+	} else {
+		if(swap_init_ok) {
+			//(1）According to the mm AND addr, try to load the content of right disk page
+			//    into the memory which page managed.
+			//(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
+			//(3) make the page swappable.
+			struct Page *page = NULL;
+			swap_in(mm, addr, &page);
+			page_insert(mm->pgdir, page, addr, perm);
+			swap_map_swappable(mm, addr, page, 1);
+			page->pra_vaddr = addr;
+		}
+		else {
+			cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+			goto failed;
+		}
+	}
    ret = 0;
 failed:
     return ret;
